5e3624e19a068a9984397ae1712ab7b016c57ea3
Nicol <nicorubb@etb.com.co>
2022-09-02 13:46:29 -0500

    Segunda Versión

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,4 +12,29 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // customOptions: OwlOptions = {
+  //   loop: true, //Si es falso solo muestra las imagenes, no las repite
+  //   mouseDrag: true, //Deja mover las imagenes deslizando el mouse
+  //   touchDrag: false,
+  //   pullDrag: true,
+  //   dots: false, //Los puntos de abajo
+  //   navSpeed: 700,
+  //   navText: [' ', ' '],//Para cambiar de imagen
+  //   responsive: { //El numero de imagenes a mostrar según el tamaño
+  //     0: {
+  //       items: 1
+  //     },
+  //     400: {
+  //       items: 1
+  //     },
+  //     740: {
+  //       items: 1
+  //     },
+  //     940: {
+  //       items: 1
+  //     }
+  //   },
+  //   nav: true
+  // }
+
 }
