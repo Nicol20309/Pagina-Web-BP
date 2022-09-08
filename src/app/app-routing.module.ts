c1683c856e144fc3561a71c6bf712d435057f531
Nicol <nicorubb@etb.com.co>
2022-09-08 12:42:16 -0500

    Implementación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgregarComponent } from './components/agregar/agregar.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { CoupensComponent } from './components/coupens/coupens.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MediaComponent } from './components/media/media.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 //Rutas de navegación
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'}, //Para que si esta vacio me lleve de una vez a la pagina de Bienvenida
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent },
   {path: 'login', component: LoginComponent },
-  {path: 'agregar', component: AgregarComponent }
+  {path: 'agregar', component: AgregarComponent },
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'coupens', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +8,36 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AgregarComponent } from './components/agregar/agregar.component';
 import { LoginComponent } from './components/login/login.component';
+import { BodyComponent } from './components/body/body.component';
+import { CoupensComponent } from './components/coupens/coupens.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MediaComponent } from './components/media/media.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AgregarComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, CarouselModule],
+  declarations: [
+    AppComponent, 
+    HomeComponent, 
+    AgregarComponent, 
+    LoginComponent, 
+    BodyComponent, 
+    CoupensComponent, 
+    DashboardComponent, 
+    MediaComponent, 
+    PagesComponent,
+    SidenavComponent, 
+    StatisticsComponent, 
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    AppRoutingModule, 
+    CarouselModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
